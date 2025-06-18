9dc3f23bb5d86c2af64cc52d9420e1a5ad243ce8
manuelsrleon <manuelsrleon@gmail.com>
2025-06-19 01:07:08 +0200

    move assets to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   <header id="main-menu" >
       <div className="menu-bg"></div>
       <div className="wrapper">
-          <a id="init" href="#init"><img className="logo-mini" src="images/bricolabs_logo_mini.png" alt="Logo Bricolabs" /></a>
+          <a id="init" href="#init"><img className="logo-mini" src="assets/images/bricolabs_logo_mini.png" alt="Logo Bricolabs" /></a>
           <div className="show-menu"></div>
           <nav className="menu">
               <ul>
@@ -37,7 +37,7 @@ function App() {
             <h1 id="hero-title">Fomentando la cultura open desde 2013 ⚡</h1>
             <h2 id="hero-subtitle">Bienvenido al primer makerspace de Galicia.</h2>
         </div>
-      <img id="hero-logo" src="src/assets/images/bricolabs_logo.png" alt="Logo Bricolabs" />
+      <img id="hero-logo" src="assets/images/bricolabs_logo.png" alt="Logo Bricolabs" />
   </section>
   <section className="description content">
       <a id="who"></a>
@@ -49,7 +49,7 @@ function App() {
                     Es un grupo de trabajo donde todo el mundo puede proponer ideas y colaborar en las diferentes fases de los proyectos, a través de reuniones altamente participativas y en un ambiente ameno y dinámico, donde no importa tanto el nivel de conocimiento como la motivación en la realización de la actividad.
               </div>
                 </div>
-          <img className="desc-image" src="src/assets/images/group_photo.jpg" alt="Foto de grupo Bricolabs" />
+          <img className="desc-image" src="assets/images/group_photo.jpg" alt="Foto de grupo Bricolabs" />
       </div>
   </section>
   <section className="proyects content">
@@ -59,27 +59,27 @@ function App() {
           <div className="section-desc">Cada semana nos reunimos para poner en común proyectos personales y desarrollar proyectos conjuntos. En estas imágenes puedes ver algunas cosas que hemos hecho. Además asistimos a numeros eventos de hardware libre y organizamos anualmente la feria de tecnología abierta OSHWDem.<br /><br />Puedes ver toda la documentación sobre nuestros proyectos en nuestra <a href="wiki/">Wiki</a>. También puedes acceder al <a href="https://github.com/brico-labs">GitHub</a> de los proyectos comunes.</div>
           <div className="img-container">
               <div className="img-desc"><h1>Concurso de robots siguelíneas</h1><br />OSHWDem 2014<br /> Museo Domus de A coruña</div>
-              <img src="src/assets/images/siguelineas-small.jpg" alt="Concurso de robots siguelíneas en la OSHWDem" />
+              <img src="assets/images/siguelineas-small.jpg" alt="Concurso de robots siguelíneas en la OSHWDem" />
           </div>
           <div className="img-container">
               <div className="img-desc"><h1>Concurso de robots sumo</h1><br />OSHWDem 2014<br /> Museo Domus de A coruña</div>
-              <img src="src/assets/images/sumo-robot-small.jpg" alt="Consurso de robots sumo en la OSHWDem" />
+              <img src="assets/images/sumo-robot-small.jpg" alt="Consurso de robots sumo en la OSHWDem" />
           </div>
           <div className="img-container">
               <div className="img-desc"><h1>Bricolabs en el día de la ciencia en la calle</h1><br />2013<br /> Parque de Santa Margarita<br /> (casa de las ciencias)</div>
-              <img src="src/assets/images/ciencia-en-la-calle-small.jpg" alt="Bricolabs en el dia de la ciencia en la calle" />
+              <img src="assets/images/ciencia-en-la-calle-small.jpg" alt="Bricolabs en el dia de la ciencia en la calle" />
           </div>
           <div className="img-container">
               <div className="img-desc"><h1>Escornabot</h1><br />Proyecto libre de robótica educativa</div>
-              <img src="src/assets/images/escornabot-small.jpg" alt="Proyecto libre de robótica educativa Escornabot" />
+              <img src="assets/images/escornabot-small.jpg" alt="Proyecto libre de robótica educativa Escornabot" />
           </div>
           <div className="img-container">
               <div className="img-desc"><h1>Sensor siguelíneas</h1><br />Fabricando un sensor para robot siguelíneas</div>
-              <img src="src/assets/images/sensor_siguelineas-small.jpg" alt="Fabricando sensor siguelineas" />
+              <img src="assets/images/sensor_siguelineas-small.jpg" alt="Fabricando sensor siguelineas" />
           </div>
           <div className="img-container">
               <div className="img-desc"><h1>Proyecto de electónica</h1><br />Jornada de "cacharreo" en Bricolabs</div>
-              <img src="src/assets/images/proyecto_electronica-small.jpg" alt="Proyecto electronica en Bricolabs" />
+              <img src="assets/images/proyecto_electronica-small.jpg" alt="Proyecto electronica en Bricolabs" />
           </div>
       </div>
   </section>
@@ -93,10 +93,10 @@ function App() {
       </div>
   </section>
   <footer>
-      <a href="https://twitter.com/brico_labs" target="_blank"><img src="src/assets/images/twitter.png" alt="Twitter" /></a>
-      <a href="https://www.flickr.com/photos/bricolabs/" target="_blank"><img src="src/assets/images/flickr.png" alt="Twitter" /></a>
-      <a href="https://github.com/brico-labs" target="_blank"><img src="src/assets/images/github-white.png" alt="GitHub" /></a>
-      <a href="mailto:contacto@bricolabs.cc"><img src="src/assets/images/email.png" alt="Email" /></a>
+      <a href="https://twitter.com/brico_labs" target="_blank"><img src="assets/images/twitter.png" alt="Twitter" /></a>
+      <a href="https://www.flickr.com/photos/bricolabs/" target="_blank"><img src="assets/images/flickr.png" alt="Twitter" /></a>
+      <a href="https://github.com/brico-labs" target="_blank"><img src="assets/images/github-white.png" alt="GitHub" /></a>
+      <a href="mailto:contacto@bricolabs.cc"><img src="assets/images/email.png" alt="Email" /></a>
   </footer>
   <div className="image-fullscreen">
   <div className="close-image"></div>
